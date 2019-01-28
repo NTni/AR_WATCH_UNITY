eaca70057bfca9a1363773f32aca16ae940280c5
unknown <ntninitin@gmail.com>
2019-01-28 23:21:25 +0530

    (watchadded)

---
+++ AR_watch/Assets/Scripts/Time_show.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Time_show : MonoBehaviour
+{
+    public Text Tyme;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int samay = 0;
+        Tyme.text = "Time: " + samay.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(System.DateTime.Now.ToString("dd-MM-yyyy"));
+        string samay = System.DateTime.Now.ToString("hh:mm:ss");
+        Debug.Log(samay);
+        Tyme.text = "Time: " + samay.ToString();
+
+    }
+}
